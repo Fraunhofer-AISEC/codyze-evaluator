45cec5f979697673ebd0b3b9e6ccdd58ee386ad9
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-14 11:54:14 +0100

    Move query to kts file

--- projects/magnum-cve/queries/apply-restrictive-file-permissions.kts
+++ projects/magnum-cve/queries/apply-restrictive-file-permissions.kts
@@ -1,3 +1,15 @@
 fun statement1(tr: TranslationResult) {
-    return QueryTree(true)
+    return tr.allExtended<WriteFile>(
+        mustSatisfy = { writeOp ->
+            executionPath(
+                startNode = writeOp,
+                type = Must,
+                direction = Backward(GraphToFollow.EOG),
+                scope = Interprocedural(),
+                predicate = {
+                    it is SetFileMask && it.mask == 0x180L /* 0x180 == 0o600 */
+                },
+            )
+        }
+    )
 }
