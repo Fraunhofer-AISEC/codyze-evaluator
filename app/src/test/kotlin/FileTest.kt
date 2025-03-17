ced54d3f75a9caad5c367e0f063d3ca87d83ec43
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2025-03-17 23:21:58 +0100

    Check for duplicate operation nodes (#89)

--- app/src/test/kotlin/FileTest.kt
+++ app/src/test/kotlin/FileTest.kt
@@ -9,6 +9,7 @@ import de.fraunhofer.aisec.cpg.graph.Interprocedural
 import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.GetSecret
 import de.fraunhofer.aisec.cpg.graph.concepts.file.SetFileMask
 import de.fraunhofer.aisec.cpg.graph.concepts.file.WriteFile
+import de.fraunhofer.aisec.cpg.graph.operationNodes
 import de.fraunhofer.aisec.cpg.passes.concepts.config.ProvideConfigPass
 import de.fraunhofer.aisec.cpg.passes.concepts.config.ini.IniFileConfigurationSourcePass
 import de.fraunhofer.aisec.cpg.passes.concepts.file.python.PythonFileConceptPass
@@ -22,8 +23,36 @@ import de.fraunhofer.aisec.openstack.passes.OsloConfigPass
 import kotlin.io.path.Path
 import kotlin.test.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class FileTest {
+    @Test
+    fun testDuplicateOperationNodes() {
+        val topLevel = Path("../external/magnum")
+        val result =
+            analyze(listOf(), topLevel, true) {
+                it.registerLanguage<PythonLanguage>()
+                it.registerLanguage<IniFileLanguage>()
+                it.registerPass<OsloConfigPass>()
+                it.registerPass<IniFileConfigurationSourcePass>()
+                it.registerPass<ProvideConfigPass>()
+                it.registerPass<PythonFileConceptPass>()
+                it.registerPass<MakeThingsWorkPrototypicallyPass>()
+                it.exclusionPatterns("tests")
+                it.softwareComponents(
+                    mutableMapOf("magnum" to listOf(topLevel.resolve("magnum").toFile()))
+                )
+                it.topLevels(mapOf("magnum" to topLevel.resolve("magnum").toFile()))
+            }
+        assertNotNull(result)
+
+        val operationNode = result.operationNodes
+        assertTrue(operationNode.isNotEmpty(), "Expected to find some `Operation` nodes.")
+        assertTrue(
+            operationNode.groupBy { it }.filter { it.value.size > 1 }.isEmpty(),
+            "Found at least two equal `Operation` nodes. Expected to not find any duplicates.",
+        )
+    }
 
     @Test
     fun testFileChmod() {
