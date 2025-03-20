44c7b192523d8b5570a39c125762699fd35300c7
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-20 17:46:55 +0100

    Fixes and updates

--- projects/BYOK/queries/key-must-be-input-to-disk-encryption.query.kts
+++ projects/BYOK/queries/key-must-be-input-to-disk-encryption.query.kts
@@ -2,62 +2,74 @@ import de.fraunhofer.aisec.cpg.*
 import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.graph.edges.*
 import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.*
+import de.fraunhofer.aisec.cpg.graph.concepts.file.*
+import de.fraunhofer.aisec.cpg.graph.concepts.logging.*
 import de.fraunhofer.aisec.cpg.graph.concepts.http.*
 import de.fraunhofer.aisec.cpg.graph.concepts.memory.*
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.*
 
+/**
+ * The following http endpoints are considered as a secure key provider:
+ * * GET /v1/secrets/{encryption_key_id}/payload in the `Component` "Barbican"
+ */
+fun HttpEndpoint.isSecureKeyProvider(): Boolean {
+    return httpMethod == HttpMethod.GET &&
+            path == "/v1/secrets/{secret_id}/payload" &&
+            this.underlyingNode?.firstParentOrNull<Component> {
+                it.name.localName == "barbican"
+            } != null
+}
+
+/**
+ * Checks if the data may leave the component via one of the following channels:
+ * - Writing to a file
+ * - Writing to a log
+ * - Printing to the console
+ * - Executing a command
+ * - Being exposed via an Http endpoint which is not explicitly whitelisted by being a "secure key provider"
+ */
+fun Node.dataLeavesComponent(): Boolean {
+    return this is WriteFile ||
+            this is LogWrite ||
+            (this is HttpEndpoint && !this.isSecureKeyProvider()) ||
+            (this is CallExpression && (this.name.localName == "println" || this.name.localName == "execute"))
+}
+
 fun statement1(tr: TranslationResult): QueryTree<Boolean> {
-    fun Node.dataLeavesComponent(): Boolean {
-        val whitelist = listOf<HttpEndpoint>()
-        // TODO: This should be replaced with the respective operations (writing to a file,
-        //  printing, executing commands, logging)
-        return ((this is CallExpression) &&
-                (this.name.localName == "write" ||
-                        this.name.localName == "println" ||
-                        this.name.localName == "execute" ||
-                        this.name.localName == "log")) || (this is HttpEndpoint && this.isSecureKeyProvider())
-    }
 
     val noKeyLeakResult =
         tr.allExtended<GetSecret> { secret ->
-            not(dataFlow(from = secret, predicate = { it.dataLeavesComponent() }))
+            not(
+                dataFlow(
+                    startNode = secret,
+                    type = May,
+                    scope = Interprocedural(),
+                    predicate = { it.dataLeavesComponent() },
+                )
+            )
         }
 
     return noKeyLeakResult
 }
 
 fun statement2(result: TranslationResult): QueryTree<Boolean> {
-    /**
-     * The following http endpoints are considered as a secure key provider:
-     * * GET /v1/secrets/{encryption_key_id}/payload in the `Component` "Barbican"
-     */
-    fun HttpEndpoint.isSecureKeyProvider(): Boolean {
-        return httpMethod == HttpMethod.GET &&
-                path == "/v1/secrets/{secret_id}/payload" &&
-                this.underlyingNode?.firstParentOrNull {
-                    it is Component && it.name.localName == "barbican"
-                } != null
-    }
-
-    val tree = result.allExtended<DiskEncryption> { encryption ->
-        encryption.key?.let { key ->
-            result
-                .existsExtended<HttpEndpoint>(HttpEndpoint::isSecureKeyProvider) {
-                    dataFlow(
-                        it,
-                        encryption,
-                        useIndexStack = true,
-                        collectFailedPaths = false,
-                    )
-                }
-                .children
-                .firstOrNull() as? QueryTree<Boolean> ?: QueryTree(false)
-        }
-            ?: QueryTree(
-                false,
-                mutableListOf(QueryTree(encryption)),
-                "encryptionOp.concept.key is null",
-            )
+    val tree =
+        result.allExtended<DiskEncryption> { encryption ->
+            encryption.key?.let { key ->
+                dataFlow(
+                    startNode = encryption,
+                    type = May,
+                    direction = Backward(GraphToFollow.DFG),
+                    sensitivities = FieldSensitive + ContextSensitive,
+                    scope = Interprocedural(),
+                    predicate = { it is HttpEndpoint && it.isSecureKeyProvider() },
+                )
+            }
+                ?: QueryTree(
+                    false,
+                    mutableListOf(QueryTree(encryption)),
+                    "encryptionOp.concept.key is null",
+                )
     }
 
     return tree
