ed42a996f05ccbbb836d82506f83753a1c2ed6d6
Immanuel Kunz <immanuel.kunz@aisec.fraunhofer.de>
2025-03-25 17:06:36 +0100

    Update security statements and queries (#105)

--- projects/BYOK/queries/key-accessible-only-by-valid-user.query.kts
+++ projects/BYOK/queries/key-accessible-only-by-valid-user.query.kts
@@ -2,48 +2,54 @@ import de.fraunhofer.aisec.cpg.TranslationResult
 import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.GetSecret
 import de.fraunhofer.aisec.cpg.graph.concepts.http.HttpEndpoint
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
+import de.fraunhofer.aisec.cpg.graph.*
+import de.fraunhofer.aisec.cpg.graph.concepts.auth.Authentication
+import de.fraunhofer.aisec.cpg.query.*
+import de.fraunhofer.aisec.cpg.*
 
+/**
+ * Access to Barbican keys must be restricted to authenticated
+ * users, i.e., each Barbican REST API that provides access to
+ * K must be connected to an Authentication concept.
+ */
 fun statement1(tr: TranslationResult): QueryTree<Boolean> {
-    fun Node.dataLeavesComponent(): Boolean {
-        val whitelist =
-            listOf<String>(
-                "barbican.api.controllers.secrets.SecretController.payload.HttpEndpoint",
-                "barbican.api.controllers.secrets.SecretController.on_get.HttpEndpoint",
+    return tr.allExtended<HttpEndpoint>(
+        // A secret reaches this endpoint.
+        sel = { endpoint ->
+            dataFlow(
+                startNode = endpoint,
+                type = May,
+                direction = Backward(GraphToFollow.DFG),
+                scope = Interprocedural(),
+                predicate = { it is GetSecret },
+            ).value
+        },
+        mustSatisfy = { endpoint ->
+            // There's some authentication for this endpoint
+            QueryTree<Boolean>(
+                value = endpoint.authentication != null,
+                children = endpoint.authentication?.let { mutableListOf(QueryTree<Authentication>(value = it)) }
+                    ?: mutableListOf(),
+                stringRepresentation = "The endpoint $endpoint requires authentication by ${endpoint.authentication}",
+                node = endpoint
             )
-
-        // TODO: This should be replaced with the respective operations (writing to a file,
-        //  printing, executing commands, logging)
-        return ((this is CallExpression) &&
-                (this.name.localName == "write" ||
-                        this.name.localName == "println" ||
-                        this.name.localName == "execute" ||
-                        this.name.localName == "log")) ||
-                (this is HttpEndpoint && this.name.toString() !in whitelist)
-    }
-
-    val noKeyLeakResult =
-        tr.allExtended<GetSecret> { secret ->
-            not(
-                dataFlow(
-                    startNode = secret,
-                    type = May,
-                    scope = Interprocedural(),
-                    predicate = { it.dataLeavesComponent() },
-                )
-            )
-       }
-
-    return noKeyLeakResult
+        })
 }
 
+/**
+ * Access to Barbican keys must be restricted to authenticated
+ * users through authorization, i.e., each operation on K must
+ * be connected to an Authorization concept.
+ */
 fun statement2(tr: TranslationResult): QueryTree<Boolean> {
     return QueryTree(true)
 }
 
+/**
+ * Given a list of user permissions P (defined in permission
+ * configs) and an authorization check A, a check is performed
+ * on P and used as input to A.
+ */
 fun statement3(tr: TranslationResult): QueryTree<Boolean> {
-    return QueryTree(true)
-}
-
-fun statement4(tr: TranslationResult): QueryTree<Boolean> {
     return QueryTree(true)
 }
--- projects/BYOK/queries/key-must-be-input-to-disk-encryption.query.kts
+++ projects/BYOK/queries/key-must-be-input-to-disk-encryption.query.kts
@@ -2,23 +2,57 @@ import de.fraunhofer.aisec.cpg.*
 import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.graph.edges.*
 import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.*
+import de.fraunhofer.aisec.cpg.graph.concepts.file.*
+import de.fraunhofer.aisec.cpg.graph.concepts.logging.*
 import de.fraunhofer.aisec.cpg.graph.concepts.http.*
 import de.fraunhofer.aisec.cpg.graph.concepts.memory.*
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.*
 
-fun statement1(result: TranslationResult): QueryTree<Boolean> {
-    /**
-     * The following http endpoints are considered as a secure key provider:
-     * * GET /v1/secrets/{encryption_key_id}/payload in the `Component` "Barbican"
-     */
-    fun HttpEndpoint.isSecureKeyProvider(): Boolean {
-        return httpMethod == HttpMethod.GET &&
-                path == "/v1/secrets/{secret_id}/payload" &&
-                this.underlyingNode?.firstParentOrNull<Component> {
-                    it.name.localName == "barbican"
-                } != null
-    }
+/**
+ * The following http endpoints are considered as a secure key provider:
+ * * GET /v1/secrets/{encryption_key_id}/payload in the `Component` "Barbican"
+ */
+fun HttpEndpoint.isSecureKeyProvider(): Boolean {
+    return httpMethod == HttpMethod.GET &&
+            path == "/v1/secrets/{secret_id}/payload" &&
+            this.underlyingNode?.firstParentOrNull<Component> {
+                it.name.localName == "barbican"
+            } != null
+}
 
+/**
+ * Checks if the data may leave the component via one of the following channels:
+ * - Writing to a file
+ * - Writing to a log
+ * - Printing to the console
+ * - Executing a command
+ * - Being exposed via an Http endpoint which is not explicitly whitelisted by being a "secure key provider"
+ */
+fun Node.dataLeavesComponent(): Boolean {
+    return this is WriteFile ||
+            this is LogWrite ||
+            (this is HttpEndpoint && !this.isSecureKeyProvider()) ||
+            (this is CallExpression && (this.name.localName == "println" || this.name.localName == "execute"))
+}
+
+fun statement1(tr: TranslationResult): QueryTree<Boolean> {
+
+    val noKeyLeakResult =
+        tr.allExtended<GetSecret> { secret ->
+            not(
+                dataFlow(
+                    startNode = secret,
+                    type = May,
+                    scope = Interprocedural(),
+                    predicate = { it.dataLeavesComponent() },
+                )
+            )
+        }
+
+    return noKeyLeakResult
+}
+
+fun statement2(result: TranslationResult): QueryTree<Boolean> {
     val tree =
         result.allExtended<DiskEncryption> { encryption ->
             encryption.key?.let { key ->
@@ -41,20 +75,21 @@ fun statement1(result: TranslationResult): QueryTree<Boolean> {
     return tree
 }
 
-fun statement2(result: TranslationResult): QueryTree<Boolean> {
-    val tree = result.allExtended<DiskEncryption> {
-        val processInput =
-            (it.underlyingNode as? CallExpression)?.argumentEdges?.get("process_input")?.end
-        if (processInput == null) {
-            QueryTree(true)
-        } else {
-            executionPath(it) { to ->
-                to is DeAllocate &&
-                        (to.what as? Reference)?.refersTo ==
-                        (processInput as? Reference)?.refersTo
-            }
+fun statement3(result: TranslationResult): QueryTree<Boolean> {
+    val tree = result.allExtended<DiskEncryption> { diskEncryption ->
+        val subQueries = diskEncryption.key?.ops?.filterIsInstance<GetSecret>()?.map { secret ->
+            secret.alwaysFlowsTo(
+                scope = Interprocedural(maxSteps = 100),
+                sensitivities = FilterUnreachableEOG + FieldSensitive + ContextSensitive,
+                predicate = { it is DeAllocate }, // Anforderung: de-allocate the data
+            )
         }
+        QueryTree(
+            subQueries?.all { it.value == true } ?: false,
+            subQueries?.map { QueryTree(it) }?.toMutableList() ?: mutableListOf(),
+            "All keys must be deleted",
+            diskEncryption
+        )
     }
-
     return tree
 }
