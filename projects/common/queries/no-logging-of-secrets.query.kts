25e18ee91543bc49f81eb8b7eb3f59f0a1fed34c
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-04-14 11:09:43 +0200

    Documentation of the query

--- projects/common/queries/no-logging-of-secrets.query.kts
+++ projects/common/queries/no-logging-of-secrets.query.kts
@@ -3,6 +3,16 @@ import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.*
 import de.fraunhofer.aisec.cpg.graph.concepts.logging.*
 
+/**
+ * Secrets must not be logged.
+ *
+ * This query checks if there is a data flow from any `Secret` Concept to a `LogWrite` Operation possible.
+ *
+ * **Important note for this query to work as intended:**
+ * The nodes implementing the `Secret` concept must have an outgoing DFG edge (typically to the underlyingNode).
+ * If this is not desired, an option might be to replace `Secret` with the `GetSecret` operation.
+ * Another option could be to use the `startNode = secret.underlyingNode`.
+ */
 fun statement1(tr: TranslationResult): QueryTree<Boolean> {
     return tr.allExtended<Secret> { secret ->
         not(
