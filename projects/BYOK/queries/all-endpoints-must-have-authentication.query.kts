db43135616daa9067d28dc03e97bea7c9b8d707c
Mathias Morbitzer <mathias.morbitzer@aisec.fraunhofer.de>
2025-04-24 16:41:46 +0200

    updated all-endpoints-must-have-authentication query

--- projects/BYOK/queries/all-endpoints-must-have-authentication.query.kts
+++ projects/BYOK/queries/all-endpoints-must-have-authentication.query.kts
@@ -1,18 +1,34 @@
 import de.fraunhofer.aisec.cpg.graph.concepts.http.HttpEndpoint
 
 /**
- * All HTTPEndpoints in /v3/ for cinder and /v1/ for barbican must have authentication
+ * This [Kotlin extension function](https://kotlinlang.org/docs/extensions.html#extension-functions)
+ * checks if the [HttpEndpoint] it is invoked on either has cinder as underlyingNode
+ * with the path "/v3" or barbican with the path "/v1".
+ *
+ * @return `true` if this [HttpEndpoint] should have authentication.
+ */
+fun HttpEndpoint.shouldHaveAuthentication(): Boolean {
+    return (this.underlyingNode?.component?.name?.localName == "cinder" &&
+            this.path.startsWith("/v3/")) ||
+            (this.underlyingNode?.component?.name?.localName == "barbican" &&
+                    this.path.startsWith("/v1/"))
+}
+
+/**
+ * All HTTPEndpoints that are private should have authentication methods
  */
 fun statement1(tr: TranslationResult): QueryTree<Boolean> {
     return tr.existsExtended<HttpEndpoint>(
         sel = { endpoint ->
-            // Filter the respective endpoints
-            (endpoint.underlyingNode?.component?.name?.localName == "cinder" &&
-                    endpoint.path.startsWith("/v3/")) ||
-                    (endpoint.underlyingNode?.component?.name?.localName == "barbican" &&
-                            endpoint.path.startsWith("/v1/"))
+            // Only endpoints that are private and therefore should have authentication
+            endpoint.shouldHaveAuthentication()
         },
-        // See if they all have authentication
-        mustSatisfy = { endpoint -> QueryTree(value = endpoint.authentication != null) },
+        // See if we find one that does not have authentication
+        mustSatisfy = { endpoint ->
+            QueryTree(
+                value = endpoint.authentication == null,
+                children = mutableListOf(QueryTree(endpoint)),
+            )
+        },
     )
 }
