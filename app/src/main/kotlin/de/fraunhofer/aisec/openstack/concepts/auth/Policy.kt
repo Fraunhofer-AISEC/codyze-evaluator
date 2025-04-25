ae52e0c32fff4f249c837e54478c2a69053597e3
Leutrim Shala <Leutrim.shala@aisec.fraunhofer.de>
2025-04-25 11:08:48 +0200

    Add Policy concept

---
+++ app/src/main/kotlin/de/fraunhofer/aisec/openstack/concepts/auth/Policy.kt
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the OpenStack Checker
+ */
+package de.fraunhofer.aisec.openstack.concepts.auth
+
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.concepts.Concept
+
+class Policy(underlyingNode: Node? = null) : Concept(underlyingNode = underlyingNode) {
+    var rule: PolicyRule? = null
+    //    val operations: MutableList<Node> = mutableListOf()
+}
+
+class PolicyRule(underlyingNode: Node? = null, val roles: Set<Role> = emptySet()) :
+    Concept(underlyingNode = underlyingNode) {}
+
+data class Role(val name: String, val requiredWith: Set<String> = emptySet())
---
+++ app/src/main/kotlin/de/fraunhofer/aisec/openstack/concepts/auth/PolicyBuilder.kt
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the OpenStack Checker
+ */
+package de.fraunhofer.aisec.openstack.concepts.auth
+
+import de.fraunhofer.aisec.cpg.graph.MetadataProvider
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.concepts.newConcept
+import de.fraunhofer.aisec.cpg.graph.concepts.policy.Policy
+import de.fraunhofer.aisec.cpg.graph.concepts.policy.PolicyRule
+import de.fraunhofer.aisec.cpg.graph.concepts.policy.Role
+
+fun MetadataProvider.newPolicy(underlyingNode: Node, connect: Boolean) =
+    newConcept(::Policy, underlyingNode = underlyingNode, connect = connect)
+
+fun MetadataProvider.newPolicyRule(
+    underlyingNode: Node,
+    concept: Policy,
+    roles: Set<Role>,
+    connect: Boolean,
+) =
+    newConcept({ PolicyRule(roles = roles) }, underlyingNode = underlyingNode, connect = connect)
+        .apply { concept.rule = this }
