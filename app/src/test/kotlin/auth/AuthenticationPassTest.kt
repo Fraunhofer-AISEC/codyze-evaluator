650d8322688a204a4a6f1a4d6c9ba52f941fa89a
Leutrim Shala <Leutrim.shala@aisec.fraunhofer.de>
2025-05-09 14:28:46 +0200

    Revert "Add query: token-based-authentication-is-set-for-all-endpoints"
    This reverts commit 5ffc24315712adbb50f56a7e7f9986fc9821fc2a.

--- app/src/test/kotlin/auth/AuthenticationPassTest.kt
+++ app/src/test/kotlin/auth/AuthenticationPassTest.kt
@@ -4,20 +4,14 @@
 package auth
 
 import analyze
-import de.fraunhofer.aisec.cpg.TranslationResult
 import de.fraunhofer.aisec.cpg.frontends.ini.IniFileLanguage
 import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguage
 import de.fraunhofer.aisec.cpg.graph.allChildrenWithOverlays
-import de.fraunhofer.aisec.cpg.graph.component
 import de.fraunhofer.aisec.cpg.graph.conceptNodes
 import de.fraunhofer.aisec.cpg.graph.concepts.auth.TokenBasedAuth
-import de.fraunhofer.aisec.cpg.graph.concepts.config.ConfigurationSource
 import de.fraunhofer.aisec.cpg.graph.concepts.http.HttpEndpoint
-import de.fraunhofer.aisec.cpg.graph.evaluate
 import de.fraunhofer.aisec.cpg.passes.concepts.config.ini.IniFileConfigurationSourcePass
-import de.fraunhofer.aisec.cpg.query.QueryTree
-import de.fraunhofer.aisec.cpg.query.allExtended
-import de.fraunhofer.aisec.cpg.query.exists
+import de.fraunhofer.aisec.openstack.passes.*
 import de.fraunhofer.aisec.openstack.passes.auth.AuthenticationPass
 import de.fraunhofer.aisec.openstack.passes.http.HttpPecanLibPass
 import de.fraunhofer.aisec.openstack.passes.http.HttpWsgiPass
@@ -25,7 +19,6 @@ import kotlin.io.path.Path
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
-import kotlin.toString
 import org.junit.jupiter.api.Test
 
 class AuthenticationPassTest {
@@ -103,79 +96,5 @@ class AuthenticationPassTest {
                 )
             }
         }
-    }
-
-    @Test
-    fun testTokenBasedAuthentication() {
-        val topLevel = Path("../projects/BYOK/components")
-        val result =
-            analyze(listOf(), topLevel, true) {
-                it.registerLanguage<PythonLanguage>()
-                it.registerLanguage<IniFileLanguage>()
-                it.registerPass<IniFileConfigurationSourcePass>()
-                it.registerPass<AuthenticationPass>()
-                it.registerPass<HttpPecanLibPass>()
-                it.registerPass<HttpWsgiPass>()
-                it.exclusionPatterns("tests", "drivers")
-                it.includePath("../external/webob")
-                it.softwareComponents(
-                    mutableMapOf(
-                        "cinder" to listOf(topLevel.resolve("cinder/cinder/api").toFile()),
-                        "barbican" to listOf(topLevel.resolve("barbican/barbican/api").toFile()),
-                        "keystonemiddleware" to
-                            listOf(
-                                Path("../external/keystonemiddleware/keystonemiddleware").toFile()
-                            ),
-                        "conf" to listOf(topLevel.resolve("conf").toFile()),
-                    )
-                )
-                it.topLevels(
-                    mapOf(
-                        "cinder" to topLevel.resolve("cinder").toFile(),
-                        "barbican" to topLevel.resolve("barbican").toFile(),
-                        "keystonemiddleware" to Path("../external/keystonemiddleware").toFile(),
-                        "conf" to topLevel.resolve("conf").toFile(),
-                    )
-                )
-            }
-
-        assertNotNull(result)
-
-        val tokenProvider = setOf("fernet", "jws")
-        val r =
-            result.allExtended<HttpEndpoint>(
-                sel = { endpoint ->
-                    endpoint.shouldHaveAuthentication() && isTokenProvider(result, tokenProvider)
-                },
-                mustSatisfy = { endpoint ->
-                    QueryTree(
-                        value = endpoint.authentication is TokenBasedAuth,
-                        children = mutableListOf(QueryTree(endpoint)),
-                    )
-                },
-            )
-        assertTrue(r.value)
-        println(r.printNicely())
-    }
-
-    fun HttpEndpoint.shouldHaveAuthentication(): Boolean {
-        return (this.underlyingNode?.component?.name?.localName == "cinder" &&
-            this.path.startsWith("/v3/")) ||
-            (this.underlyingNode?.component?.name?.localName == "barbican" &&
-                this.path.startsWith("/v1/"))
-    }
-
-    fun isTokenProvider(tr: TranslationResult, provider: Set<String>): Boolean {
-        return tr.exists<ConfigurationSource>(
-                sel = { config -> config.name.toString() == "keystone.conf" },
-                mustSatisfy = { config ->
-                    config.groups
-                        .flatMap { group ->
-                            group.options.filter { it.name.localName == "provider" }
-                        }
-                        .singleOrNull { it.evaluate().toString() in provider } != null
-                },
-            )
-            .first
     }
 }
