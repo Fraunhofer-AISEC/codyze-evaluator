6fc39066526332a9f1a947b2be86e167e0ffcfda
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-06-03 20:08:36 +0200

    More documentation of the assessment

--- examples/evaluate-hardened-openstack/assessment.codyze.kts
+++ examples/evaluate-hardened-openstack/assessment.codyze.kts
@@ -6,26 +6,45 @@ package example
 project {
     manualAssessment {
         of("Contributor-Diversity-Nova") {
+            /**
+             * The expected number of contributors for the nova project. This number is based on the
+             * ecosystem analysis of the OpenStack project.
+             */
             val expectedValue = 2
+
+            /**
+             * The actual number of contributors for the nova project. This number is based on the
+             * ecosystem analysis of the OpenStack project.
+             */
             val actualValue = 14
 
-            /* The expected value for contributor diversity is >= 2, so the KPI is fulfilled for
-            the nova Flamingo release */
+            // The expected value for contributor diversity is >= 2, so the KPI is fulfilled for the
+            // nova Flamingo release
             actualValue > expectedValue
         }
 
         of("Release-Reviewers-Nova") {
+            /**
+             * The expected number of reviewers involved in one release of the nova project. This
+             * number is based on the ecosystem analysis of the OpenStack project.
+             */
             val expectedValue = 10
+
+            /**
+             * The actual number of reviewers involved in one release of the nova project. This
+             * number is based on the ecosystem analysis of the OpenStack project.
+             */
             val actualValue = 27
 
-            /* Number of reviewers involved in one release is expected to be >= 10, so the KPI is
-            fulfilled for the nova Flamingo release */
+            // Number of reviewers involved in one release is expected to be >= 10, so the KPI is
+            // fulfilled for the nova Flamingo release
             actualValue > expectedValue
         }
 
         of("Branch-Protection") {
-            /* Branch protection mechanisms, including code approvals, automated gate tests, and automated testcases,
-            are active in Gerrit, so the KPI is fulfilled for the nova Flamingo release */
+            // Branch protection mechanisms, including code approvals, automated gate tests, and
+            // automated testcases, are  active in Gerrit, so the KPI is fulfilled for the nova
+            // Flamingo release
             true
         }
     }
