d624099668c36e33e363fe1e910f1deeb834958e
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-06-05 16:52:27 +0200

    Formatting

--- codyze-query-catalog/src/main/kotlin/de/fraunhofer/aisec/codyze/queries/file/TemporaryFiles.kt
+++ codyze-query-catalog/src/main/kotlin/de/fraunhofer/aisec/codyze/queries/file/TemporaryFiles.kt
@@ -1,8 +1,10 @@
+/*
+ * This file is part of the OpenStack Checker
+ */
 import de.fraunhofer.aisec.cpg.TranslationResult
 import de.fraunhofer.aisec.cpg.graph.Forward
 import de.fraunhofer.aisec.cpg.graph.GraphToFollow
 import de.fraunhofer.aisec.cpg.graph.Interprocedural
-import de.fraunhofer.aisec.cpg.graph.concepts.file.FileTempStatus
 import de.fraunhofer.aisec.cpg.graph.concepts.file.DeleteFile
 import de.fraunhofer.aisec.cpg.graph.concepts.file.OpenFile
 import de.fraunhofer.aisec.cpg.query.Must
@@ -10,20 +12,17 @@ import de.fraunhofer.aisec.cpg.query.QueryTree
 import de.fraunhofer.aisec.cpg.query.allExtended
 import de.fraunhofer.aisec.cpg.query.executionPath
 
-/**
- * for every temporary file, there is an execution flow to a delete file
- */
+/** for every temporary file, there is an execution flow to a delete file */
 context(TranslationResult)
 fun temporaryFilesAreAlwaysDeleted(): QueryTree<Boolean> {
     val tr = this@TranslationResult
-    return tr.allExtended<OpenFile>(
-        sel = {it.file.isTempFile == FileTempFileStatus.TEMP_FILE}
-    ) { openFile ->
+    return tr.allExtended<OpenFile>(sel = { it.file.isTempFile == FileTempFileStatus.TEMP_FILE }) {
+        openFile ->
         executionPath(
             startNode = openFile,
             direction = Forward(GraphToFollow.EOG),
             type = Must,
-            scope = Interprocedural()
+            scope = Interprocedural(),
         ) {
             it is DeleteFile && openFile.file == it.file
         }
