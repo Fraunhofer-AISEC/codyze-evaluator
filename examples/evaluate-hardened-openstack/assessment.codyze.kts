41801c7b9632bea767cea7b97b4dc5d091f991f7
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2025-06-10 18:00:16 +0200

    add ecosystem analysis summary

--- examples/evaluate-hardened-openstack/assessment.codyze.kts
+++ examples/evaluate-hardened-openstack/assessment.codyze.kts
@@ -16,10 +16,10 @@ project {
              * The actual number of contributors for the nova project. This number is based on the
              * ecosystem analysis of the OpenStack project.
              */
-            val actualValue = 14
+            val actualValue = 12
 
             // The expected value for contributor diversity is >= 2, so the KPI is fulfilled for the
-            // nova Flamingo release
+            // nova 2024.2 release
             actualValue > expectedValue
         }
 
@@ -34,17 +34,17 @@ project {
              * The actual number of reviewers involved in one release of the nova project. This
              * number is based on the ecosystem analysis of the OpenStack project.
              */
-            val actualValue = 27
+            val actualValue = 45
 
             // Number of reviewers involved in one release is expected to be >= 10, so the KPI is
-            // fulfilled for the nova Flamingo release
+            // fulfilled for the nova 2024.2 release
             actualValue > expectedValue
         }
 
         of("Branch-Protection") {
             // Branch protection mechanisms, including code approvals, automated gate tests, and
             // automated testcases, are  active in Gerrit, so the KPI is fulfilled for the nova
-            // Flamingo release
+            // 2024.2 release
             true
         }
 
