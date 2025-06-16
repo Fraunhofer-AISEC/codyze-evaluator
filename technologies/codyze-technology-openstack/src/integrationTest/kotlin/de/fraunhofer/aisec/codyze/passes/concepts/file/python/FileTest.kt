aae1b07a4010acc6850b683430f9015cc3cc9e0f
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-06-16 14:22:51 +0200

    Fix tests

--- technologies/codyze-technology-openstack/src/integrationTest/kotlin/de/fraunhofer/aisec/codyze/passes/concepts/file/python/FileTest.kt
+++ technologies/codyze-technology-openstack/src/integrationTest/kotlin/de/fraunhofer/aisec/codyze/passes/concepts/file/python/FileTest.kt
@@ -5,16 +5,19 @@ package de.fraunhofer.aisec.codyze.passes.concepts.file.python
 
 import de.fraunhofer.aisec.codyze.analyze
 import de.fraunhofer.aisec.codyze.passes.concepts.config.openstack.OsloConfigPass
-import de.fraunhofer.aisec.codyze.passes.openstack.MakeThingsWorkPrototypicallyPass
 import de.fraunhofer.aisec.codyze.profiles.openstack.Magnum
+import de.fraunhofer.aisec.codyze.profiles.openstack.decryptedCertToSecret
+import de.fraunhofer.aisec.codyze.profiles.openstack.getSecretPluginCall
 import de.fraunhofer.aisec.cpg.frontends.ini.IniFileLanguage
 import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguage
 import de.fraunhofer.aisec.cpg.graph.concepts.Operation
 import de.fraunhofer.aisec.cpg.graph.concepts.file.File
 import de.fraunhofer.aisec.cpg.graph.operationNodes
+import de.fraunhofer.aisec.cpg.passes.concepts.TagOverlaysPass
 import de.fraunhofer.aisec.cpg.passes.concepts.config.ProvideConfigPass
 import de.fraunhofer.aisec.cpg.passes.concepts.config.ini.IniFileConfigurationSourcePass
 import de.fraunhofer.aisec.cpg.passes.concepts.file.python.PythonFileConceptPass
+import de.fraunhofer.aisec.cpg.passes.concepts.tag
 import kotlin.io.path.Path
 import kotlin.test.*
 
@@ -39,8 +42,17 @@ class FileTest {
                 it.registerPass<IniFileConfigurationSourcePass>()
                 it.registerPass<ProvideConfigPass>()
                 it.registerPass<PythonFileConceptPass>()
-                it.registerPass<MakeThingsWorkPrototypicallyPass>()
                 it.exclusionPatterns("tests")
+                it.registerPass<TagOverlaysPass>()
+                it.configurePass<TagOverlaysPass>(
+                            TagOverlaysPass.Configuration(
+                                tag {
+                                    // Use a predefined tagging profile for secret definitions in OpenStack.
+                                    decryptedCertToSecret()
+                                    getSecretPluginCall()
+                                }
+                            )
+                        )
                 it.softwareComponents(
                     mutableMapOf("magnum" to listOf(topLevel.resolve("magnum").toFile()))
                 )
